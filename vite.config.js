115a6f8b055d2450467b11df53a79bd82c2661d3
DavidSchuchert <Davidschuchert@icloud.com>
2024-11-11 13:54:15 +0100

    deleted faker

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/dashboard/index.css",
+                "resources/css/inventar/index.css",
+                "resources/css/mitglieder/index.css",
+                "resources/css/zahlungen/create.css",
+                "resources/css/zahlungen/edit.css",
+                "resources/css/zahlungen/index.css",
+                "resources/css/zahlungen/alert.css",
+                "resources/css/zahlungen/app.css",
+                "resources/css/zahlungen/header.css",
+                "resources/css/zahlungen/search.css",
+                "resources/js/app.js",
             ],
             refresh: true,
         }),
